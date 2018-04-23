dcb1a2ab5c6e19c61eaf3f38a3c2c9f84db34f68
Miguel <rendonmg@gmail.com>
2018-04-22 20:16:48 -0500

    MONGO_URL Heroku (borrado contraseña)

--- server/config/config.js
+++ server/config/config.js
@@ -23,7 +23,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    //urlDB = 'mongodb://cafe-user:123456@ds157667.mlab.com:57667/cafe';
     urlDB = process.env.MONGO_URL;
 }
 
